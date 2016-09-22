d49717908cdf0ddaa9fd5234e2fc385650cb35e4
Pragith <pragith@gmail.com>
2016-09-22 17:46:17 +0530

    module2-solution complete.

--- module2-solution/js/app.js
+++ module2-solution/js/app.js
@@ -15,14 +15,11 @@
       shopping.buy_items = ShoppingListService.viewItems()[0]
       shopping.addToBoughtList = function(item_index){
         ShoppingListService.buyItem(item_index)
-        //shopping.item_count = (shopping.buy_items.length == 0 ? true : false)
       }
-      shopping.item_count = shopping_service.shopping_counter
   }
 
   function AlreadyBoughtShoppingControllerFn(ShoppingListService){
       this.bought_items = ShoppingListService.viewItems()[1]
-      this.item_count = !shopping_service.bought_counter
   }
 
 
@@ -38,16 +35,10 @@
     ]
 
     bought_items = []
-    shopping_service.bought_counter = false
 
     shopping_service.buyItem = function(item_index){
       bought_items.push(shopping_items[item_index]) // Add to "Bought" list
       shopping_items.splice(item_index, 1) // Remove from "To Buy" list
-
-      shopping_service.bought_counter = (bought_items.length == 1 ? false : true)
-      shopping_service.shopping_counter = (shopping_items.length == 0 ? true : false)
-      console.log('Bought length:', bought_items.length, shopping_service.bought_counter)
-      console.log('Shopping length:', shopping_items.length, shopping_service.shopping_counter)
     }
 
     shopping_service.viewItems = function(){
